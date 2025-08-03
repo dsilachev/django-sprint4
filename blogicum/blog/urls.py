2ece9db3d93b8e0f7f51e67e6337d4838e5a26f7
Дмитрий Силачёв <dsilachev@Presidio.local>
2025-08-03 16:59:36 +0300

    Changes after PEP8

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -4,52 +4,56 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
-    # Главная страница
     path('', views.PostListView.as_view(), name='index'),
 
-    # Посты
     path('posts/', include([
-        path('create/',
-            views.PostCreateView.as_view(),
-            name='create_post'),
+        path('create/', views.PostCreateView.as_view(), name='create_post'),
 
         path('<int:post_id>/', include([
-            path('',
-                views.PostDetailView.as_view(),
-                name='post_detail'),
-            path('edit/',
-                views.PostUpdateView.as_view(),
-                name='edit_post'),
-            path('delete/',
+            path('', views.PostDetailView.as_view(), name='post_detail'),
+            path('edit/', views.PostUpdateView.as_view(), name='edit_post'),
+            path(
+                'delete/',
                 views.PostDeleteView.as_view(),
-                name='delete_post'),
+                name='delete_post'
+            ),
 
-            path('comment/',
+            path(
+                'comment/',
                 views.CommentCreateView.as_view(),
-                name='add_comment'),
+                name='add_comment'
+            ),
 
-            path('edit_comment/<int:comment_id>/',
+            path(
+                'edit_comment/<int:comment_id>/',
                 views.CommentUpdateView.as_view(),
-                name='edit_comment'),
+                name='edit_comment'
+            ),
 
-            path('delete_comment/<int:comment_id>/',
+            path(
+                'delete_comment/<int:comment_id>/',
                 views.CommentDeleteView.as_view(),
-                name='delete_comment'),
+                name='delete_comment'
+            ),
         ])),
     ])),
 
-    # Профили
     path('profile/', include([
-        path('<str:username>/',
+        path(
+            '<str:username>/',
             views.ProfileListView.as_view(),
-            name='profile'),
-        path('',
+            name='profile'
+        ),
+        path(
+            '',
             views.ProfileUpdateView.as_view(),
-            name='edit_profile'),
+            name='edit_profile'
+        ),
     ])),
 
-    # Категории
-    path('category/<slug:category_slug>/',
+    path(
+        'category/<slug:category_slug>/',
         views.CategoryListView.as_view(),
-        name='category_posts'),
+        name='category_posts'
+    ),
 ]
